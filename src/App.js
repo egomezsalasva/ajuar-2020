26bc8abce298a02a00fab6a303c33bef06f556b4
egomezsalasva <egomezsalasva@gmail.com>
2020-12-17 15:40:57 +0100

    Set up branding styles file and structure page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+import { fonts, colors } from './brandingStyles'
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  
+  .headerContainer{
+    background: orange;
+    position: absolute;
+    top: 25px;
+    width: 100%;
+    text-align: center;
+    color: #fff;
+  }
+`
 
 const App = () => {
   return (
-    <>
-    </>
+    <AppContainer>
+      <div className="headerContainer">
+        <a href="https://www.instagram.com/ajuarestudio/" target="_blank" rel="noreferrer" className="headerLink">@AJUARESTUDIO</a>
+      </div>
+      
+    </AppContainer>
   )
 }
 export default App
---
+++ src/brandingStyles.js
@@ -0,0 +1,12 @@
+
+const fonts = {
+    display: 'Cako',
+    text: 'Suisse Intl',
+}
+
+const colors = {
+    dark: "#000",
+    light: "#fff",
+}
+
+export { fonts, colors } 
